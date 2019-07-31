f63c433df7f3d63fb570ee9f7afb3345eecb39a3
Chasecooler <chasemarq@yahoo.com>
2019-07-31 14:41:16 -0400

    first commit redo

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from "./ScrollToTop"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { RoomProvider } from "./context"
+
+ReactDOM.render(
+
+  <RoomProvider>
+
+    <Router>
+
+      <ScrollToTop>
+
+        <App />
+
+      </ScrollToTop>
+
+    </Router>
+
+  </RoomProvider>,
+
+  document.getElementById("root")
+
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
